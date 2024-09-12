81d2d180c1547d492bff1962a808d14458de3e0e
naveenkumar <naveenkumar@metamation.com>
2024-09-12 22:07:23 +0530

    skipUntracked implemented and initialized other skip options but needs change

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace BackupGitRepo;
 
 class Program {
    static string sRepositoryDir = "", sBackupDir = "", sGitStatusFilePath = "";
+   static bool sSkipUntracked = false, sSkipStaged = false, sSkipAdded = false, sSkipDeleted = false, sSkipModified = false;
 
    static void Main (string[] args) {
       if (args.Length == 0) {
@@ -14,6 +15,33 @@ class Program {
          appName += ".exe";
          Console.WriteLine ($"{appName} <RepoDirectoryPath> [<BackupDirectoryPath>]");
          Environment.Exit (-1);
+      }
+
+      // -su - skipUntracked
+      // -ss - skipStaged
+      // -sa - skipAdded
+      // -sd - skipDeleted
+      // -sm - skipModified
+      // BackupGitRepo -su -ss -sd <> <>
+      if(args[0].StartsWith("-su", StringComparison.OrdinalIgnoreCase)) {
+         args = args.Skip (1).ToArray ();
+         sSkipUntracked = true;
+      }
+      if (args[0].StartsWith ("-ss", StringComparison.OrdinalIgnoreCase)) {
+         args = args.Skip (1).ToArray ();
+         sSkipStaged = true;
+      }
+      if (args[0].StartsWith ("-sa", StringComparison.OrdinalIgnoreCase)) {
+         args = args.Skip (1).ToArray ();
+         sSkipAdded = true;
+      }
+      if (args[0].StartsWith ("-sd", StringComparison.OrdinalIgnoreCase)) {
+         args = args.Skip (1).ToArray ();
+         sSkipDeleted = true;
+      }
+      if (args[0].StartsWith ("-sm", StringComparison.OrdinalIgnoreCase)) {
+         args = args.Skip (1).ToArray ();
+         sSkipModified = true;
       }
 
       sRepositoryDir = Path.GetFullPath (GetCurrentDirectoryOrArgument (args, 0));
@@ -50,37 +78,136 @@ class Program {
          using var repo = new Repository (sRepositoryDir); // Gets repo from the path to .git
 
          #region RepoChangeLog
-         var sb = new StringBuilder ();
+         var sbStaged = new StringBuilder ();
+         var sbUnstaged = new StringBuilder ();
+         var sbUntracked = new StringBuilder ();
+         var sbConflicted = new StringBuilder ();
+
          // Retrieves git status without including .gitignored files
-         var files = repo.RetrieveStatus (new StatusOptions { IncludeIgnored = false });
+         var files = repo.RetrieveStatus (new StatusOptions { IncludeIgnored = false, IncludeUntracked = true });
+
+         foreach (var file in files) {
+            var state = file.State;
+
+            var isStaged = state.HasFlag (FileStatus.NewInIndex) || state.HasFlag (FileStatus.ModifiedInIndex) || state.HasFlag (FileStatus.RenamedInIndex) || state.HasFlag (FileStatus.DeletedFromIndex) || state.HasFlag (FileStatus.TypeChangeInIndex);
+            var isUnstaged = state.HasFlag (FileStatus.ModifiedInWorkdir) || state.HasFlag (FileStatus.RenamedInWorkdir) || state.HasFlag (FileStatus.DeletedFromWorkdir) || state.HasFlag (FileStatus.TypeChangeInWorkdir);
+            var isUntracked = state.HasFlag (FileStatus.NewInWorkdir);
+            var isConflicted = state.HasFlag (FileStatus.Conflicted);
+
+            var s = GetFileStatusName (state); // Gets status name - New/Modified/Deleted/Renamed/TypeChanged
+
+            if (isStaged || isUnstaged) {
+               sbStaged.AppendLine ($"{s} - '{file.FilePath}'");
+            }
+            //if (isUnstaged) {
+            //   sbUnstaged.AppendLine ($"{s} - '{file.FilePath}'");
+            //}
+            if (isUntracked) {
+               if (!file.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase)) {
+                  sbUntracked.AppendLine ($"{s} - '{file.FilePath}'");
+               }
+            }
+            //if (isConflicted) {
+            //   sbConflicted.AppendLine ($"{s} - '{file.FilePath}'");
+            //}
+         }
+
+         var sb = new StringBuilder ();
          sb.AppendLine ($"BRANCH: {repo.Head.FriendlyName}\n");
-         if (files.Staged.Count () > 0) {
+         if (sbStaged.Length > 0) {
+            sb.AppendLine ("CHANGES IN TRACKED FILES: ********************************************************");
+            sb.AppendLine (sbStaged.ToString () + "\n");
+         }
+         //if (sbUnstaged.Length > 0) {
+         //   sb.AppendLine ("UNSTAGED: ********************************************************");
+         //   sb.AppendLine (sbUnstaged.ToString () + "\n");
+         //}
+         if (sbUntracked.Length > 0) {
+            sb.AppendLine ("UNTRACKED: ********************************************************");
+            sb.AppendLine (sbUntracked.ToString () + "\n");
+         }
+         //if (sbConflicted.Length > 0) {
+         //   sb.AppendLine ("CONFLICTED: ********************************************************");
+         //   sb.AppendLine (sbConflicted.ToString () + "\n");
+         //}
+
+         File.WriteAllText (sGitStatusFilePath, sb.ToString ());
+         Console.WriteLine (sb.ToString ());
+         /*
+
+         #region Staged
+
+         if (!sSkipStaged && files.Staged.Count () > 0) {
             sb.AppendLine ("********************************************************");
             sb.AppendLine ("STAGED FILES:");
-            foreach (var f in files.Staged) sb.AppendLine (f.FilePath);
+            foreach (var f in files.Staged) {
+               var s = GetFileStatus (f);
+               sb.AppendLine ($"{s} - '{f.FilePath}'");
+            }
          }
-         if (files.Modified.Count () > 0) {
+
+         //if (!sSkipAdded && files.Added.Count () > 0) {
+         //   sb.AppendLine ("********************************************************");
+         //   sb.AppendLine ("ADDED FILES:");
+         //   foreach (var f in files.Added) {
+         //      if (!f.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase))
+         //         sb.AppendLine (f.FilePath);
+         //   }
+         //}
+
+         //if (!sSkipDeleted && files.Removed.Count () > 0) {
+         //   sb.AppendLine ("********************************************************");
+         //   sb.AppendLine ("REMOVED FILES:");
+         //   foreach (var f in files.Removed) {
+         //      if (!f.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase))
+         //         sb.AppendLine (f.FilePath);
+         //   }
+         //}
+
+         #endregion
+
+         #region Unstaged
+
+         if (!sSkipModified && files.Modified.Count () > 0) {
             sb.AppendLine ("********************************************************");
-            sb.AppendLine ("MODIFIED FILES:");
-            foreach (var f in files.Modified) sb.AppendLine (f.FilePath);
+            sb.AppendLine ("UNSTAGED FILES:");
+            foreach (var f in files.Modified) {
+               var s = GetFileStatus (f);
+               sb.AppendLine ($"{s} - '{f.FilePath}'");
+            }
          }
-         if (files.Untracked.Count () > 0) {
+
+         #endregion
+
+         #region Deleted
+
+         if (!sSkipModified && files.Missing.Count () > 0) {
+            sb.AppendLine ("********************************************************");
+            sb.AppendLine ("MISSING FILES:");
+            foreach (var f in files.Missing) {
+               var s = GetFileStatus (f);
+               sb.AppendLine ($"{s} - '{f.FilePath}'");
+            }
+         }
+
+         #endregion
+
+         #region Untracked
+
+         if (!sSkipUntracked && files.Untracked.Count () > 0) {
             sb.AppendLine ("********************************************************");
             sb.AppendLine ("UNTRACKED FILES:");
             foreach (var f in files.Untracked) {
-               if (!f.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase))
-                  sb.AppendLine (f.FilePath);
-            }
-         }  
-         if (files.Added.Count () > 0) {
-            sb.AppendLine ("********************************************************");
-            sb.AppendLine ("ADDED FILES:");
-            foreach (var f in files.Added) {
-               if (!f.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase))
-                  sb.AppendLine (f.FilePath);
+               if (!f.FilePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase)) {
+                  var s = GetFileStatus (f);
+                  sb.AppendLine ($"{s} - '{f.FilePath}'");
+               }
             }
          }
-         File.WriteAllText (sGitStatusFilePath, sb.ToString ());
+
+         #endregion
+
+         */
          #endregion
 
          #region BackupRepoFiles
@@ -89,8 +216,11 @@ class Program {
             if (item.State != FileStatus.Unaltered) { // git ignored files are not considered by default. // item.State != FileStatus.Ignored
                // checks for files in the parent folder/subfolders.
                var filePath = item.FilePath.NormalizePathSeparators ();
+               Console.WriteLine ($"{item.State} - {filePath}");
                if (!filePath.StartsWith (backupRelativeDir, StringComparison.OrdinalIgnoreCase)) {
+                  if (sSkipUntracked && item.State.HasFlag (FileStatus.NewInWorkdir)) continue; // Skips untracked files if required
                   string srcFile = Path.Combine (sRepositoryDir, item.FilePath);
+                  if (!File.Exists (srcFile)) continue; // checks if the file is not deleted
                   string destFilePath = Path.Combine (sBackupDir, item.FilePath);
                   var dirName = Path.GetDirectoryName (destFilePath) ?? "";
                   if (!string.IsNullOrEmpty (dirName)) Directory.CreateDirectory (dirName);
@@ -134,4 +264,39 @@ class Program {
       File.WriteAllText (sGitStatusFilePath, ""); // Creating file with empty content.
       return defaultBackupDir;
    }
+
+   static string GetFileStatus (StatusEntry file) {
+      return file.State switch {
+         FileStatus.NewInIndex
+            or FileStatus.NewInWorkdir
+            or (FileStatus.NewInIndex | FileStatus.ModifiedInWorkdir)
+            or (FileStatus.NewInIndex | FileStatus.NewInWorkdir) => "[ADDED]",
+
+         FileStatus.ModifiedInIndex
+            or FileStatus.ModifiedInWorkdir
+            or (FileStatus.ModifiedInIndex | FileStatus.ModifiedInWorkdir) => "[MODIFIED]",
+
+         FileStatus.DeletedFromIndex
+            or FileStatus.DeletedFromWorkdir
+            or FileStatus.Nonexistent
+            or FileStatus.NewInIndex | FileStatus.DeletedFromWorkdir
+            or (FileStatus.DeletedFromIndex | FileStatus.DeletedFromWorkdir) => "[DELETED]",
+
+         FileStatus.RenamedInIndex | FileStatus.RenamedInWorkdir | (FileStatus.RenamedInIndex | FileStatus.RenamedInWorkdir) => "[RENAMED]",
+
+         _ => $"[{file.State.ToString ()}]" // Default
+      };
+   }
+
+   static string GetFileStatusName (FileStatus fileStatus) {
+      return fileStatus switch {
+         _ when fileStatus.HasFlag (FileStatus.NewInWorkdir) => "[ADDED]",
+         _ when fileStatus.HasFlag (FileStatus.ModifiedInWorkdir) => "[MODIFIED]",
+         _ when fileStatus.HasFlag (FileStatus.RenamedInWorkdir) => "[RENAMED]",
+         _ when fileStatus.HasFlag (FileStatus.DeletedFromWorkdir) => "[DELETED]",
+         _ when fileStatus.HasFlag (FileStatus.TypeChangeInWorkdir) => "[TYPECHANGED]",
+         _ => string.Join (" | ", Enum.GetValues (typeof (FileStatus)).Cast<FileStatus> ().Where (f => fileStatus.HasFlag (f)).Select (f => f))
+      };
+   }
+
 }
