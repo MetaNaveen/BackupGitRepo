4107e9b4bea068d488840fc32e1b7d8b219b7587
naveenkumar <naveenkumar@metamation.com>
2024-09-18 17:05:24 +0530

    Added missed files

---
+++ Utils/AssemblyUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Resources;
+
+namespace Utils;
+
+public class AssemblyUtils {
+   public static (string Name, int Major, int Minor, int Revision) GetAssemblyVersion () {
+      Assembly? currentAssembly = Assembly.GetEntryAssembly (); // EXE name could be anything. //Assembly.GetExecutingAssembly ();
+      AssemblyName assemblyName = currentAssembly!.GetName ();
+      Version? currentVersion = assemblyName.Version;
+      return (assemblyName.Name!, currentVersion!.Major, currentVersion.Minor, currentVersion.Revision);
+   }
+
+   public static string GetResXFromEmbeddedResource (string resourceFile, string resourceKey) {
+      Assembly currentAssembly = Assembly.GetExecutingAssembly ()!;
+      ResourceManager rm = new ($"{currentAssembly!.GetName ().Name}.Resources.{resourceFile}", currentAssembly);
+      string secretKey = rm.GetString (resourceKey)!;
+      return secretKey;
+   }
+}
